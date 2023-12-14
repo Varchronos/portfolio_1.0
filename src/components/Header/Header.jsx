4f236245dec3deceb3a7d73276ffddc2b079cbaa
lcs2020062@iiitl.ac.in <lcs2020062@iiitl.ac.in>
2023-12-14 17:05:24 +0530

    minor changes to header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
                 <h1 className={styles.heading}>I am A Developer</h1>
                 <h4 className={styles.tagline}>Always learning something new, every week <span>(take breaks)</span></h4>
                 <div className={styles.homeButtons}>
-                    <button className={styles.emailBtn}><a href="#footer" className={styles.link}>email me</a></button>
-                    <button className={styles.workBtn}><a href="#work" className={styles.link}>my work</a></button>
+                    <a href="#footer" className={styles.emailBtn}>email me</a>
+                    <a href="#work" className={styles.workBtn}>my work</a>
                 </div>
                 </div>
             </section>
